ba302491e0a42058436b00bbee6da09026967c13
smirnova <dddsssmmm555@mail.ru>
2021-06-28 10:02:37 +0300

    add changes

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -3,13 +3,23 @@ package spring_introduction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //
-//@Component
+@Component
 public class Person {
 
     private Pet pet;
 //
 //    @Autowired
     public Person(Pet pet) {
+        System.out.println("Person is created");
+        this.pet = pet;
+    }
+
+//    public Pet getPet() {
+//        return pet;
+//    }
+
+    @Autowired
+    public void setPet(Pet pet) {
         this.pet = pet;
     }
 
--- src/main/java/spring_introduction/Test.java
+++ src/main/java/spring_introduction/Test.java
@@ -1,19 +1,19 @@
-package spring_introduction;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class Test {
-
-    public static void main(String[] args) {
-
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        Cat cat = context.getBean("catBean", Cat.class);
-        cat.say();
-
-        Dog dog = context.getBean("dog", Dog.class);
-        dog.say();
-
-        context.close();
-    }
-}
+//package spring_introduction;
+//
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//public class Test {
+//
+//    public static void main(String[] args) {
+//
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//        Cat cat = context.getBean("catBean", Cat.class);
+//        cat.say();
+//
+//        Dog dog = context.getBean("dog", Dog.class);
+//        dog.say();
+//
+//        context.close();
+//    }
+//}
