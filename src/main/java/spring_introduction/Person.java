c4df23099b7ab559aa7f67381f77e5c60e63430c
smirnova <dddsssmmm555@mail.ru>
2021-06-28 10:10:50 +0300

    add changes

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,20 +2,22 @@ package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-//
+
 @Component
 public class Person {
 
+    @Autowired
     private Pet pet;
-//
+
 //    @Autowired
     public Person(Pet pet) {
         System.out.println("Person is created");
         this.pet = pet;
     }
 
-    @Autowired
+//    @Autowired
     public void setPet(Pet pet) {
+        System.out.println("Class Person: set Pet");
         this.pet = pet;
     }
 
